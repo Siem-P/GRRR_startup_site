0d103a4fbf9f29b93ea0be2ce70512cb65f2bf62
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-01-24 16:51:35 +0100

    Mobile version 0.4

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,36 +1,19 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-// Company name - Landing Page
-gsap.from(".company-name", {
-    y: -200,
-    ease: Power3.easeOut,
-    duration: 2,
-    delay: 2,
-});
-
-
-// H1 - Landing view 
 gsap.from(".hero-intro h1", {
     clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
     duration: 1.2,
     delay: .5,
 });
 
-// 
-// gsap.to(".company-name", {
-//     scrollTrigger: {
-//         trigger: ".advertisement-section",
-//         markers: true,
-//         start: "top 10%",
-//         end: "bottom 100%",
-//         scrub: true,
-//     },
-//     color: "var(--neon-green)",
-//     backgroundColor: "var(--black)",
-//     duration: 1
-    
-// })
+// Company name - Landing Page
+gsap.from(".company-name", {
+    x: -300,
+    ease: Power3.easeOut,
+    duration: 2,
+    delay: 1,
+});
 
 // Five-Zero's
 gsap.from(".five-zero-section > span", {
@@ -58,6 +41,7 @@ gsap.from("#whos-behind-this-article .portfolio-image", {
         // markers: true,
         toggleActions: "play none none pause",
     },
+    stagger: 0.5,
     x: 500,
     duration: 1,
     ease: Power2.easeOut
